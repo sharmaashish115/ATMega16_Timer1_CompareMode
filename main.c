a5539b129f023170f746ffb6a6cc7cf39e090cc7
Ashish Sharma <ashish823397@gmail.com>
2023-06-07 21:49:44 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,38 +1,55 @@
 /*
- * ATMega16_Timer0.c
+ * ATMega16_Timer1_CompareMode.c
  *
- * Created: 2023-06-02 12:06:27 PM
+ * Created: 2023-06-07 10:23:24 AM
  * Author : a_shi
  */ 
+
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-volatile uint8_t count;
+//Clear Timer 1 on Compare Mode "Output Compare A/B Match Interrupt Enable"
+
+void timer1_init(void)
+{
+	TCCR1B |= (1<<WGM12) | (1<<CS12);		//CTC mode is enabled, Prescaler is set to 256
+	TCNT1 = 0;								//Initialize the timer 1.
+	OCR1A = 31250;
+	
+	/*
+	Compare Register Count Calculation
+	
+	Clock Source -> 8MHz external crystal.
+	Use Prescaler 256.
+	
+	Clock freq -> 8000000/256 = 31250Hz
+	
+	Time period = 1/31250 = 0.000032sec = 0.032millisec
+	
+	To blink an LED at 1Hz i.e. 1sec = 1000msec (1000msec in 1sec)
+	
+	To elapse 0.032 milliseconds, it takes 1 count
+	So, to elapse 1 milliseconds, it takes 1/0.032count
+	So, to elapse 1000 milliseconds, it takes {(1/32)*1000} = 31250 count
+	
+	*/
+	
+	TIMSK |= (1<<OCIE1A);		//Timer Counter 1, Output Compare A Match Interrupt Enable.
+	sei();						//Global Interrupt Enable.
+}
 
 int main(void)
 {
 	DDRD |= (1<<6);
-	TCNT0 =0;
-	count =0;
-	TCCR0 &= ~(1<<CS01);
-	TCCR0 |= (1<<CS02) | (1<<CS00);
-	TIMSK |= 1<<TOIE0;
-	sei();
-		
+	timer1_init();
+	
     while (1) 
     {
     }
 }
 
-ISR(TIMER0_OVF_vect)
+ISR (TIMER1_COMPA_vect)
 {
-	if (count==31)
-	{
-		PORTD ^= (1<<6);
-		count=0;
-	}
-	else
-	count++;
-	
+	PORTD ^= (1<<6);
 }
